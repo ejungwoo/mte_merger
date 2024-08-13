bb158148c8658795e67cba3c8d6526ff1c4d6443
Jung Woo Lee <phyjics@gmail.com>
2024-08-13 22:39:14 +0900

    rearanged codes

--- LKMTEMerger.h
+++ LKMTEMerger.h
@@ -4,51 +4,78 @@
 #include "TTree.h"
 #include <vector>
 
-#define NUM_MTE_INPUT_CHANNELS 10
+#define NUM_MTE_SOURCES 10
+
+#ifndef LILAK_VERSION
+#define e_info cout<<"\033[0;32m==\033[0m "
+#define e_error cout<<"\033[0;31mER\033[0m "
+#define e_test cout<<"\033[0;36mtest\033[0m "
+//#define coutn cout<<"\033[0;36mNT\033[0m "
+//#define couti cout<<"\033[0;32m==\033[0m "
+//#define coutw cout<<"\033[0;33mWR\033[0m "
+//#define coute cout<<"\033[0;31mER\033[0m "
+#endif
 
 /// Master trigger electronics
 class LKMTEMerger
 {
     public:
-        LKMTEMerger();
+        LKMTEMerger(TString outputFileName);
         virtual ~LKMTEMerger() {};
 
         void SetKeyChannel(int no, TString name);
         void SetInputChannel(int no, TString name);
-        bool ReadMTE(TString inputFileName, TString outputFileName);
+
+        bool ReadMTE(TString inputFileName);
         bool MapKobra(TString fileName, TString kobraName="Kobra");
-        void GetTimeOffset();
-        void FindTimeOffset();
+
+        void GetTimeOffset (TString fileName="data/mte_time_offset.txt");
+        void FindTimeOffset(TString fileName="data/mte_time_offset.txt");
+
         void WriteSummaryTree();
         bool ReadKobra(TString fileName, TString kobraName="Kobra");
 
         int GetKobraEntry(int i);
 
+    public:
+        void SetOffsetFileName(TString name) { fOffsetFileName = name; }
+        void SetTimeWindowFactor(double factor) { fTimeWindowFactor = factor; }
+
     private:
-        int trig_num;
-        double trig_time;
-        int trig_type;
-
-        bool fInitialized = false;
-
-        TTree* fMTETree[NUM_MTE_INPUT_CHANNELS];
-        int fMTEEntries[NUM_MTE_INPUT_CHANNELS];
-        int fMaxEntries = 0;
-        TTree* fCoboTree;
+        TString fOutputFileName = "";
 
         int fKeyChannelNumber;
-        TString fInputChannelName[NUM_MTE_INPUT_CHANNELS];
+        TString fInputChannelName[NUM_MTE_SOURCES];
         std::vector<int> fInputChannelArray;
+        int fEntryOffset[NUM_MTE_SOURCES] = {0};
+        double fMTETimeOffset[NUM_MTE_SOURCES] = {0};
+
+        // MTE tree
+        int    bTrigNum = 0;
+        int    bTrigType = 0;
+        double bTrigTime = 0;
+
+        // Summary tree
+        int    bKeyNum  = 0;
+        double bKeyTime = 0;
+        int    bKeyType = 0;
+        int    bMTEEntry[NUM_MTE_SOURCES] = {0};
+        int    bDAQEntry[NUM_MTE_SOURCES] = {0};
+
+        TTree* fMTETree[NUM_MTE_SOURCES];
+        int fMTEEntries[NUM_MTE_SOURCES];
+        int fMaxEntries = 0;
+
+        double fTimeWindowFactor = 1./50000;
 
         TFile *fOutputFile = nullptr;
         TTree *fTreeSummary = nullptr;
 
-        int fNumEntriesKobra = 0;
+        TString fOffsetFileName = "data/mte_time_offset.txt";
 
-        int fEntryOffset[NUM_MTE_INPUT_CHANNELS] = {0};
-        double fMTETimeOffset[NUM_MTE_INPUT_CHANNELS] = {0};
-
-    //ClassDef(LKMTEMerger, 1)
+#ifndef LILAK_VERSION
+    ClassDef(LKMTEMerger, 1)
+#endif
 };
 
 #endif
--- read_mte.C
+++ read_mte.C
@@ -1,22 +1,21 @@
+//#include "LKLogger.h"
 #include "LKMTEMerger.cpp"
 
 void read_mte()
 {
-    auto merger = new LKMTEMerger();
+    auto merger = new LKMTEMerger("data/mte_summary.root");
+
     merger -> SetKeyChannel(0,"Cobo");
     merger -> SetInputChannel(1,"Kobra");
     merger -> SetInputChannel(2,"Pulser");
     merger -> SetInputChannel(3,"Scint");
-    merger -> ReadMTE("data/lte_tag_128.dat","data/mte_summary.root");
-    //merger -> FindTimeOffset();
-    merger -> GetTimeOffset();
+
+    merger -> ReadMTE("data/lte_tag_128.dat");
+    //merger -> FindTimeOffset("data/mte_time_offset.txt");
+    merger -> GetTimeOffset("data/mte_time_offset.txt");
     merger -> MapKobra("data/output00581.root");
     merger -> WriteSummaryTree();
     merger -> ReadKobra("data/output00581.root");
 
-    auto koboEntry = merger -> GetKobraEntry(1);
-    cout << koboEntry << endl;
-
-    //merger -> ReadKobra("data/output00581.root"); // write file name to summary file
-    //merger -> ReadCobo("data/cobo.root");
+    //auto koboEntry = merger -> GetKobraEntry(1);
 }
